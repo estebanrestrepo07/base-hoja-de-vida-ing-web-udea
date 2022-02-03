3f6bc4d7dbcad6106e318ad81bc7e8913fd56299
Esteban Restrepo <esteban.restrepo@amadeus.com>
2022-02-02 23:34:28 -0500

    fix deploy in heroku app

--- src/Layouts/PublicLayout.jsx
+++ src/Layouts/PublicLayout.jsx
@@ -3,16 +3,14 @@ import { Outlet } from 'react-router-dom';
 // import Footer from '../components/Footer';
 import Navbar from 'components/Navbar';
 
-const PublicLayout = () => {
-  return (
-    <div className='flex flex-col'>
-      <Navbar />
-      <div className='h-full'>
-        <Outlet />
-      </div>
-      {/* <Footer /> */}
+const PublicLayout = () => (
+  <div className='flex flex-col'>
+    <Navbar />
+    <div className='h-full'>
+      <Outlet />
     </div>
-  );
-};
+    {/* <Footer /> */}
+  </div>
+);
 
 export default PublicLayout;
--- src/components/TagsContent.jsx
+++ src/components/TagsContent.jsx
@@ -9,16 +9,14 @@ const TagContent = (props) => {
       </h3>
 
       <div className='tagContent-list flex flex-col md:flex-row md:flex-wrap'>
-        {tags.map((tag) => {
-          return (
-            <div
-              className={`h-10 flex text-white font-semibold items-center justify-center w-auto my-2 min-w-36 border rounded-tl-full rounded-br-full px-4 py-6 ${tag.bgColor}`}
-              key={tag.id}
-            >
-              {tag.value}
-            </div>
-          );
-        })}
+        {tags.map((tag) => (
+          <div
+            className={`h-10 flex text-white font-semibold items-center justify-center w-auto my-2 min-w-36 border rounded-tl-full rounded-br-full px-4 py-6 ${tag.bgColor}`}
+            key={tag.id}
+          >
+            {tag.value}
+          </div>
+        ))}
       </div>
     </div>
   );
